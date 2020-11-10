b8d0ca5d19258dc98125f659b1e2c50f02392855
Jon Froehlich <jonfroehlich@gmail.com>
2020-11-10 06:41:40 -0800

    added auto-resize and fullscreen options to sidebar

--- DOM/CollapsibleSidebar2-WithP5js/sketch.js
+++ DOM/CollapsibleSidebar2-WithP5js/sketch.js
@@ -1,8 +1,12 @@
-var _ballSize = 50;
-var _ballFillColor = null;
+let _ballSize = 50;
+let _ballFillColor = null;
+let DEFAULT_HEIGHT = 400;
+let DEFAULT_WIDTH = 400;
+let _autoFillDiv = false;
+var canvas = null;
 
 function setup() {
-  let canvas = createCanvas(400, 400);
+  canvas = createCanvas(DEFAULT_WIDTH, DEFAULT_HEIGHT);
 
   // Move the canvas so it’s inside our <div id="sketch-container">.
   canvas.parent('sketch-container');
@@ -16,7 +20,9 @@ function setup() {
 
   let hexColor = "#" + hex(red(_ballFillColor),2) + hex(green(_ballFillColor),2) + hex(blue(_ballFillColor),2);
   document.getElementById("color-picker").value = hexColor;
-  noLoop();
+  
+  // Since there is no animation, we only need to redraw when we have a new event
+  //noLoop();
 }
 
 function draw() {
@@ -25,18 +31,46 @@ function draw() {
   circle(width/2, height/2, _ballSize);
 }
 
-function sizeChanged(src, e){
-  print("sizeChanged event: ");
-  print(src);
-  print(e);
+function ballSizeChanged(src, e){
+  print("ballSizeChanged", src, e);
   _ballSize = src.value;
   redraw();
 }
 
-function colorChanged(src, e){
-  print("colorChanged event: ");
-  print(src);
-  print(e);
+function ballColorChanged(src, e){
+  print("ballColorChanged", src, e);
   _ballFillColor = src.value;
   redraw();
+}
+
+function autoFillDivChanged(src, e){
+  print("autoFillDivChanged", src, e);
+  _autoFillDiv = src.checked;
+  resizeCanvasEvent();
+}
+
+function fullScreenModeChanged(src, e){
+  print("fullScreenModeChanged", src, e);
+  fullscreen(src.checked);
+  resizeCanvasEvent();
+  redraw();
+}
+
+function resizeCanvasEvent(){
+  if(_autoFillDiv){
+    // assigns new values for width and height variables
+    w = window.innerWidth;
+    h = window.innerHeight;  
+    // canvas.size(w,h);
+    resizeCanvas(w, h);
+    print(w, h);
+  }else{
+    resizeCanvas(DEFAULT_WIDTH, DEFAULT_HEIGHT);
+  }
+  redraw();
+}
+
+window.onresize = function() {
+  print("onresize event");
+  resizeCanvasEvent();
 }
