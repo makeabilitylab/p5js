2a8e7371260b465a37f69a67d320c22a71895bfa
Jon Froehlich <jonfroehlich@gmail.com>
2020-11-19 11:02:14 -0800

    added TODOs for basic serial I/O example with p5js

--- WebSerial/Basic-WithP5js/sketch.js
+++ WebSerial/Basic-WithP5js/sketch.js
@@ -1,5 +1,15 @@
+let slider;
+
 function setup() {
   createCanvas(400, 400);
+  
+  slider = createSlider(0, 255, 100);
+  slider.position(10, 10);
+  slider.style('width', '80px');
+
+  // TODO:
+  // add in circle that resizes with slider position
+  // add in serial connectivity
 }
 
 function draw() {
