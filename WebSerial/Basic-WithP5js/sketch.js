7cfb4aea55b21ca51169271048b9a216bf829baa
Jon Froehlich <jonfroehlich@gmail.com>
2020-11-25 05:13:26 -0800

    starting to work on p5js web serial example

--- WebSerial/Basic-WithP5js/sketch.js
+++ WebSerial/Basic-WithP5js/sketch.js
@@ -6,13 +6,20 @@ function setup() {
   slider = createSlider(0, 255, 100);
   slider.position(10, 10);
   slider.style('width', '80px');
+  slider.input(onSliderValueChanged)
 
   // TODO:
   // add in circle that resizes with slider position
   // add in serial connectivity
 }
 
+function onSliderValueChanged(){
+  console.log("Slider:", slider.value());
+  
+  
+}
+
 function draw() {
   background(220);
-  
+  text(slider.value(), 100, 24);
 }
