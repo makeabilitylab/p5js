22e0374fd967d8d4a6bc716ab15c74fcfb1662b7
Jon Froehlich <jonfroehlich@gmail.com>
2020-12-30 05:30:55 -0800

    switching computers...

--- Color/ColorExplorer3D/sketch.js
+++ Color/ColorExplorer3D/sketch.js
@@ -79,18 +79,22 @@ class ColorPanel2D {
 
     this.colorAxes = colorAxes2D;
     this.selectedColor = color(0);
+
+    this.offscreenBuffer = createGraphics(width, height);
+    this.updateOffscreenBuffer();
   }
 
-  getRight(){
+  getRight() {
     return this.x + this.width;
   }
 
-  getBottom(){
+  getBottom() {
     return this.y + this.height;
   }
 
   /**
-   * Returns the closest pixel to the given color
+   * Returns the closest pixel (x, y) coordinate to the given color. Pixel coordinates are relative 
+   * to the color panel so 0,0 is top-left of this panel.
    * 
    * @param {p5.Color} col  
    */
@@ -98,11 +102,26 @@ class ColorPanel2D {
 
   }
 
-  getColorForPixel(x, y) {
-    let x_c = map(x, this.x, this.getRight(), 0, 255);
+  /**
+   * Calculates the color for pixel x, y. By default, pixel coordinates are relative to the color panel
+   * so 0,0 is top-left of this panel. If you want to use global coordinates for the p5js app,
+   * then set useGlobalCoordinates to true (it's false by default)
+   * 
+   * @param {Number} x the x coord relative to the color panel (assumes 0,0 is top-left)
+   * @param {Number} y the y coord relative to the color panel (assumes 0,0 is top-left)
+   * @param {boolean} useGlobalCoordinates whether to use global coordinates
+   */
+  getColorForPixel(x, y, useGlobalCoordinates = false) {
+    let x_c, y_c;
 
-    // TODO: have to use getBottom()
-    let y_c = map(y, this.height, this.y, 0, 255);
+    if (useGlobalCoordinates) {
+      x_c = map(x, this.x, this.getRight(), 0, 255);
+      y_c = map(y, this.getBottom(), this.y, 0, 255);
+     
+    } else {
+      x_c = map(x, 0, this.width, 0, 255);
+      y_c = map(y, this.height, 0, 0, 255);
+    }
 
     let other_c;
     switch (this.colorAxes) {
@@ -116,23 +135,31 @@ class ColorPanel2D {
         other_c = red(this.selectedColor);
         return color(other_c, y_c, x_c);
     }
+  }
 
+  updateOffscreenBuffer() {
+    if (this.width != this.offscreenBuffer.width || this.height != this.offscreenBuffer.height) {
+      this.offscreenBuffer = createGraphics(width, height);
+    }
 
+    this.offscreenBuffer.push();
+    this.offscreenBuffer.strokeWeight(1);
+    // TODO: to speed this up, consider acessing pixels directly via getPixels()?
+    for (let x = 0; x < this.width; x++) {
+      for (let y = 0; y < this.height; y++) {
+        let c = this.getColorForPixel(x, y);
+        this.offscreenBuffer.stroke(c);
+        this.offscreenBuffer.point(x, y);
+      }
+    }
+
+    this.offscreenBuffer.pop();
   }
 
   draw() {
 
-    // TODO: to speed this up, access pixels directly OR paint via CreateGraphics
-    push();
-    strokeWeight(1);
-    for (let x = this.x; x < this.getRight(); x++) {
-      for (let y = this.y; y < this.getBottom(); y++) {
-        let c = this.getColorForPixel(x, y);
-        stroke(c);
-        point(x, y);
-      }
-    }
-    pop();
+
+    image(this.offscreenBuffer, this.x, this.y);
 
   }
 }
--- README.md
+++ README.md
@@ -8,3 +8,7 @@ Live link: https://makeabilitylab.github.io/p5js/
 - Nice video on how to use visual studio code with p5js: https://youtu.be/MOLp_zZ7Dqg
 - Get intellisense in p5js and VS Code: https://breaksome.tech/p5js-editor-how-to-set-up-visual-studio-code/. More on this here: https://github.com/processing/p5.js/issues/1339 and for `jsconfig.json` here: https://code.visualstudio.com/docs/languages/jsconfig
 -- Alternatively, maybe use this extension instead: https://marketplace.visualstudio.com/items?itemName=samplavigne.p5-vscode
+
+## Resources
+- Shiffman's videos
+- Justin Bakse's ["Computational Form" course](https://compform.net/) at the New School
