310a7bcd25dbbb4afcf839c6c33dfa6c7ad61221
Jon Froehlich <jonfroehlich@gmail.com>
2021-01-02 07:26:59 -0800

    added hover support to color cube and updated selection rendering

--- Color/ColorCube3D/color-cube-3d.js
+++ Color/ColorCube3D/color-cube-3d.js
@@ -1,25 +1,25 @@
 
 // Enum formulation from: https://stackoverflow.com/a/44447975/388117
-const SelectedColorBehavior = Object.freeze({
-  SHOW_RGB_PLANES: Symbol("Show selected R, G, B planes"),
-  SHOW_RGB_COLUMNS: Symbol("Show selected R, G, B columns"),
-  SHOW_RGB_COLUMNS_BEFORE: Symbol("Show selected R, G, B columns (before)"),
-  SHOW_CUBE_BEFORE: Symbol("Show the cube (before)"),
-  SHOW_JUST_CUBE: Symbol("Just show selected cube"),
-  SHOW_ALL: Symbol("Show all cubes")
+const SelectedColorRenderBehavior = Object.freeze({
+  // SHOW_RGB_PLANES: Symbol("Show selected R, G, B planes"),
+  SHOW_SELECTED_COLUMNS: Symbol("Show selected R, G, B columns"),
+  SHOW_SELECTED_COLUMNS_BEFORE: Symbol("Show selected R, G, B columns (before)"),
+  // SHOW_CUBE_BEFORE: Symbol("Show the cube (before)"),
+  SHOW_SELECTED_CUBE: Symbol("Just show selected cube"),
+  // SHOW_ALL: Symbol("Show all cubes")
 });
 
-class ColorPanel3D extends ColorPanel{
-  constructor(x, y, z, width, height, depth){
+class ColorPanel3D extends ColorPanel {
+  constructor(x, y, z, width, height, depth) {
     super(x, y, width, height);
     this.depth = depth;
     this.z = z;
   }
 }
 
-class ColorCube3D extends ColorPanel3D{
+class ColorCube3D extends ColorPanel3D {
   constructor(x, y, z, numCols = 10, boxSize = 10, boxMargin = 2) {
-  
+
     let cubeSize = ColorCube3D.calcCubeSize(numCols, boxSize, boxMargin);
     super(x, y, z, cubeSize, cubeSize, cubeSize);
 
@@ -27,17 +27,18 @@ class ColorCube3D extends ColorPanel3D{
     this.boxMargin = boxMargin;
     this.numCols = numCols;
     this.maxColor = 255;
- 
+
     this.defaultBoxOpacity = 255;
     this.selectedBoxOpacity = 255;
 
-    this.selectedColorBehavior = SelectedColorBehavior.SHOW_RGB_COLUMNS_BEFORE;
-    this.hoverColorBehavior = SelectedColorBehavior.SHOW_RGB_COLUMNS_BEFORE; 
+    this.selectedColorBehavior = SelectedColorRenderBehavior.SHOW_SELECTED_COLUMNS;
+    this.hoverColorBehavior = SelectedColorRenderBehavior.SHOW_SELECTED_COLUMNS;
 
     this.forceSelectedColorsToClosestMatch = true;
+
   }
 
-  getColorStep(){
+  getColorStep() {
     return this.maxColor / this.numCols;
   }
 
@@ -46,19 +47,62 @@ class ColorCube3D extends ColorPanel3D{
    * @param {p5.Color} c 
    */
   getClosestColor(c) {
-    let numGradations = this.numCols;
     let colorStep = this.getColorStep();
-    let r = floor(map(red(c), 0, 255, 0, numGradations));
-    let g = floor(map(green(c), 0, 255, 0, numGradations));
-    let b = floor(map(blue(c), 0, 255, 0, numGradations));
-    return color(r * colorStep, g * colorStep, b * colorStep, alpha(c));
+    let cube = this.getCubeForColor(c);
+    return color(cube[0] * colorStep, cube[1] * colorStep, cube[2] * colorStep, alpha(c));
   }
-  
-  setSelectedColor(selectedColor){
+
+  /**
+   * Returns the cube location (xCol, yCol, zCol) as an array
+   * @param {Array} c 
+   */
+  getCubeForColor(c) {
+    let xCol = floor(map(red(c), 0, 255, 0, this.numCols));
+    let yCol = floor(map(green(c), 0, 255, 0, this.numCols));
+    let zCol = floor(map(blue(c), 0, 255, 0, this.numCols));
+    return [xCol, yCol, zCol];
+  }
+
+  /**
+   * Gets the color for the cube
+   * @param {Array} cube 
+   */
+  getColorForCube(cube) {
+    let r = map(cube[0], 0, this.numCols, 0, 255);
+    let g = map(cube[1], 0, this.numCols, 0, 255);
+    let b = map(cube[2], 0, this.numCols, 0, 255);
+    return [r, g, b];
+  }
+
+  /**
+   * Gets the cube location for color
+   * @param {p5.Color} c 
+   */
+  getCubeLocationForColor(c) {
+    let cube = this.getCubeForColor(c);
+    return this.getCubeLocationForCube(cube);
+  }
+
+  /**
+   * Gets the cube x,y,z location for the cube coordinates (xCol, yCol, zCol)
+   * @param {Array} cube 
+   */
+  getCubeLocationForCube(cube) {
+    let x = cube[0] * (this.boxSize + this.boxMargin);
+    let y = -cube[1] * (this.boxSize + this.boxMargin);
+    let z = cube[2] * (this.boxSize + this.boxMargin);
+    return [x, y, z];
+  }
+
+  /**
+   * Sets the selected color
+   * @param {p5.Color or Array} selectedColor 
+   */
+  setSelectedColor(selectedColor) {
     // the super call parses different forms of colors
     super.setSelectedColor(selectedColor);
 
-    if(this.forceSelectedColorsToClosestMatch){
+    if (this.forceSelectedColorsToClosestMatch) {
       let closestColor = this.getClosestColor(this.selectedColor);
       super.setSelectedColor(closestColor);
     }
@@ -124,124 +168,217 @@ class ColorCube3D extends ColorPanel3D{
   }
 
   draw() {
+    //print(this.selectedColor, this.getCubeForColor(this.selectedColor));
+    //print(this.selectedColor, this.getCubeLocationForColor(this.selectedColor));
+    const hoverColorStroke = color(255);
+    this.drawSelection(this.selectedColor, this.selectedColorBehavior, false);
+    this.drawSelection(this.hoverColor, this.hoverColorBehavior, true);
+  }
 
-    // Note: box(x, y, z) expects the center x, center y, and center z
-    // It's like drawing rect with rectMode(CENTER) in 2D
-    let colorStep = this.getColorStep();
+  drawSelection(selColor, renderBehavior, isHover) {
 
-    // draw the giant rgb 3d color grid
-    for (let r = 0; r <= this.maxColor; r += colorStep) {
-      for (let g = 0; g <= this.maxColor; g += colorStep) {
-        for (let b = 0; b <= this.maxColor; b += colorStep) {
-          let x = (r / colorStep) * (this.boxSize + this.boxMargin);
-          let y = -(g / colorStep) * (this.boxSize + this.boxMargin); // green axis goes up
-          let z = (b / colorStep) * (this.boxSize + this.boxMargin);
+    let selCubeLoc = this.getCubeLocationForColor(selColor);
+    let selCube = this.getCubeForColor(selColor);
 
-          push();
-          translate(x, y, z);
-          noStroke();
+    switch (renderBehavior) {
+      case SelectedColorRenderBehavior.SHOW_SELECTED_CUBE:
+        this.drawCube(selCubeLoc, selColor, isHover, true);
+        break;
+      case SelectedColorRenderBehavior.SHOW_SELECTED_COLUMNS_BEFORE:
+        this.drawColumn(0, selCube, isHover);
+        this.drawColumn(1, selCube, isHover);
+        this.drawColumn(2, selCube, isHover);
+        this.drawCube(selCubeLoc, selColor, isHover, true);
+        break;
+      case SelectedColorRenderBehavior.SHOW_SELECTED_COLUMNS:
+        this.drawColumn(0, selCube, isHover, 0, this.numCols + 1);
+        this.drawColumn(1, selCube, isHover, 0, this.numCols + 1);
+        this.drawColumn(2, selCube, isHover, 0, this.numCols + 1);
+        this.drawCube(selCubeLoc, selColor, isHover, true);
+        break;
+    }
 
-          //uncomment below to draw unselected boxes as skeleton
-          //stroke(r, g, b, this.defaultBoxOpacity);
-          //noFill();
-          //box(boxSize);
 
-          switch (this.selectedColorBehavior) {
-            case SelectedColorBehavior.SHOW_RGB_PLANES:
-              if (this.matchesSelectedColor(r, g, b, 1)) {
-                fill(r, g, b, this.defaultBoxOpacity);
 
-                if (this.matchesSelectedColor(r, g, b)) {
-                  fill(r, g, b, this.selectedBoxOpacity);
-                  stroke(0);
-                }
 
-                box(this.boxSize);
-              }
-              break;
+  }
 
-            case SelectedColorBehavior.SHOW_RGB_PLANES_BEFORE:
-              // TODO: still need to fix this
-              let rDist = abs(red(this.selectedColor) - r);
-              let gDist = abs(green(this.selectedColor) - g);
-              let bDist = abs(green(this.selectedColor) - b);
-              if (this.matchesSelectedColor(r, g, b, 1) && r <= red(this.selectedColor) && g <= green(this.selectedColor) && b <= blue(this.selectedColor)) {
-                //print(rDist, gDist, bDist);
-                fill(r, g, b, this.defaultBoxOpacity);
-
-                if (this.matchesSelectedColor(r, g, b)) {
-                  fill(r, g, b, this.selectedBoxOpacity);
-                  stroke(0);
-                }
-
-                box(this.boxSize);
-
-              }
-              break;
-            case SelectedColorBehavior.SHOW_RGB_COLUMNS:
-              if (this.matchesSelectedColor(r, g, b, 2)) {
-
-                fill(r, g, b, this.defaultBoxOpacity);
-
-                if (this.matchesSelectedColor(r, g, b)) {
-                  fill(r, g, b, this.selectedBoxOpacity);
-                  stroke(0);
-                }
-
-                box(this.boxSize);
-              }
-              break;
-            case SelectedColorBehavior.SHOW_RGB_COLUMNS_BEFORE:
-              if (this.matchesSelectedColor(r, g, b, 2) && (r <= red(this.selectedColor) &&
-                g <= green(this.selectedColor) && b <= blue(this.selectedColor))) {
-
-                fill(r, g, b, this.defaultBoxOpacity);
-
-                if (this.matchesSelectedColor(r, g, b)) {
-                  fill(r, g, b, this.selectedBoxOpacity);
-                  stroke(0);
-                }
-
-                box(this.boxSize);
-              }
-              break;
-            case SelectedColorBehavior.SHOW_CUBE_BEFORE:
-              if (r <= red(this.selectedColor) && g <= green(this.selectedColor) && b <= blue(this.selectedColor)) {
-
-                fill(r, g, b, this.defaultBoxOpacity);
-
-                if (this.matchesSelectedColor(r, g, b)) {
-                  fill(r, g, b, this.selectedBoxOpacity);
-                  stroke(0);
-                }
-
-                box(this.boxSize);
-              }
-              break;
-            case SelectedColorBehavior.SHOW_ALL:
-              if (this.matchesSelectedColor(r, g, b)) {
-                fill(r, g, b, this.selectedBoxOpacity);
-                stroke(0);
-              } else {
-                fill(r, g, b, this.defaultBoxOpacity);
-              }
-              box(this.boxSize);
-              break;
-            case SelectedColorBehavior.SHOW_JUST_CUBE:
-            default:
-              if (this.matchesSelectedColor(r, g, b)) {
-                fill(r, g, b, this.selectedBoxOpacity);
-                stroke(0);
-                box(this.boxSize);
-              }
-              break;
-          }
-
-          pop();
-        }
+  drawCube(cubeLoc, fillColor, isHover, isSelected) {
+    push();
+    translate(cubeLoc[0], cubeLoc[1], cubeLoc[2]);
+    if (isSelected) {
+      if (isHover) {
+        stroke(255);
+      } else {
+        stroke(255);
       }
+    } else {
+      noStroke();
+    }
+
+    if (isHover) {
+      // can draw hover cubes differently...
+
+      // for example, as wireframes rather than fills
+      // stroke(fillColor);
+      // noFill();
+
+      // experimented with using alpha
+      // fillColor = color(red(fillColor), green(fillColor), blue(fillColor), 180);
+      fill(fillColor);
+    } else {
+      fill(fillColor);
+    }
+
+    box(this.boxSize);
+    pop();
+  }
+
+  /**
+   * Draws the column up to (but not including) the given cube column coordinates (xCol, yCol, zCol)
+   * @param {Integer} columnAxis either 0 (for x), 1 (for y), or 2 (for z)
+   * @param {Array} selCube Array of xCol, yCol, zCol
+   */
+  drawColumn(columnAxis, selCube, isHover, startIndex = 0, endIndex = -1) {
+    if(endIndex == -1){
+      endIndex = selCube[columnAxis]
+    }
+    for (let columnIndex = startIndex; columnIndex < endIndex; columnIndex++) {
+      let cube = null;
+      if (columnAxis === 0) {
+        cube = [columnIndex, selCube[1], selCube[2]];
+      } else if (columnAxis === 1) {
+        cube = [selCube[0], columnIndex, selCube[2]];
+      }
+      else if (columnAxis === 2) {
+        cube = [selCube[0], selCube[1], columnIndex];
+      }
+
+      let cubeLoc = this.getCubeLocationForCube(cube);
+      let c = this.getColorForCube(cube);
+      this.drawCube(cubeLoc, c, isHover, false);
     }
   }
+
+  // draw() {
+
+  //   // Note: box(x, y, z) expects the center x, center y, and center z
+  //   // It's like drawing rect with rectMode(CENTER) in 2D
+  //   let colorStep = this.getColorStep();
+
+  //   // draw the giant rgb 3d color grid
+  //   for (let r = 0; r <= this.maxColor; r += colorStep) {
+  //     for (let g = 0; g <= this.maxColor; g += colorStep) {
+  //       for (let b = 0; b <= this.maxColor; b += colorStep) {
+  //         let x = (r / colorStep) * (this.boxSize + this.boxMargin);
+  //         let y = -(g / colorStep) * (this.boxSize + this.boxMargin); // green axis goes up
+  //         let z = (b / colorStep) * (this.boxSize + this.boxMargin);
+
+  //         push();
+  //         translate(x, y, z);
+  //         noStroke();
+
+  //         //uncomment below to draw unselected boxes as skeleton
+  //         //stroke(r, g, b, this.defaultBoxOpacity);
+  //         //noFill();
+  //         //box(boxSize);
+
+  //         switch (this.selectedColorBehavior) {
+  //           case SelectedColorBehavior.SHOW_RGB_PLANES:
+  //             if (this.matchesSelectedColor(r, g, b, 1)) {
+  //               fill(r, g, b, this.defaultBoxOpacity);
+
+  //               if (this.matchesSelectedColor(r, g, b)) {
+  //                 fill(r, g, b, this.selectedBoxOpacity);
+  //                 stroke(0);
+  //               }
+
+  //               box(this.boxSize);
+  //             }
+  //             break;
+
+  //           case SelectedColorBehavior.SHOW_RGB_PLANES_BEFORE:
+  //             // TODO: still need to fix this
+  //             let rDist = abs(red(this.selectedColor) - r);
+  //             let gDist = abs(green(this.selectedColor) - g);
+  //             let bDist = abs(green(this.selectedColor) - b);
+  //             if (this.matchesSelectedColor(r, g, b, 1) && r <= red(this.selectedColor) && g <= green(this.selectedColor) && b <= blue(this.selectedColor)) {
+  //               //print(rDist, gDist, bDist);
+  //               fill(r, g, b, this.defaultBoxOpacity);
+
+  //               if (this.matchesSelectedColor(r, g, b)) {
+  //                 fill(r, g, b, this.selectedBoxOpacity);
+  //                 stroke(0);
+  //               }
+
+  //               box(this.boxSize);
+
+  //             }
+  //             break;
+  //           case SelectedColorBehavior.SHOW_RGB_COLUMNS:
+  //             if (this.matchesSelectedColor(r, g, b, 2)) {
+
+  //               fill(r, g, b, this.defaultBoxOpacity);
+
+  //               if (this.matchesSelectedColor(r, g, b)) {
+  //                 fill(r, g, b, this.selectedBoxOpacity);
+  //                 stroke(0);
+  //               }
+
+  //               box(this.boxSize);
+  //             }
+  //             break;
+  //           case SelectedColorBehavior.SHOW_RGB_COLUMNS_BEFORE:
+  //             if (this.matchesSelectedColor(r, g, b, 2) && (r <= red(this.selectedColor) &&
+  //               g <= green(this.selectedColor) && b <= blue(this.selectedColor))) {
+
+  //               fill(r, g, b, this.defaultBoxOpacity);
+
+  //               if (this.matchesSelectedColor(r, g, b)) {
+  //                 fill(r, g, b, this.selectedBoxOpacity);
+  //                 stroke(0);
+  //               }
+
+  //               box(this.boxSize);
+  //             }
+  //             break;
+  //           case SelectedColorBehavior.SHOW_CUBE_BEFORE:
+  //             if (r <= red(this.selectedColor) && g <= green(this.selectedColor) && b <= blue(this.selectedColor)) {
+
+  //               fill(r, g, b, this.defaultBoxOpacity);
+
+  //               if (this.matchesSelectedColor(r, g, b)) {
+  //                 fill(r, g, b, this.selectedBoxOpacity);
+  //                 stroke(0);
+  //               }
+
+  //               box(this.boxSize);
+  //             }
+  //             break;
+  //           case SelectedColorBehavior.SHOW_ALL:
+  //             if (this.matchesSelectedColor(r, g, b)) {
+  //               fill(r, g, b, this.selectedBoxOpacity);
+  //               stroke(0);
+  //             } else {
+  //               fill(r, g, b, this.defaultBoxOpacity);
+  //             }
+  //             box(this.boxSize);
+  //             break;
+  //           case SelectedColorBehavior.SHOW_JUST_CUBE:
+  //           default:
+  //             if (this.matchesSelectedColor(r, g, b)) {
+  //               fill(r, g, b, this.selectedBoxOpacity);
+  //               stroke(0);
+  //               box(this.boxSize);
+  //             }
+  //             break;
+  //         }
+
+  //         pop();
+  //       }
+  //     }
+  //   }
+  // }
 
   keyPressed() {
     let colorStep = this.getColorStep();
@@ -278,7 +415,13 @@ class ColorCube3D extends ColorPanel3D{
     this.fireNewSelectedColorEvent(newSelectedColor);
   }
 
-  static calcCubeSize(numCols, boxSize, boxMargin){
+  static calcCubeSize(numCols, boxSize, boxMargin) {
     return numCols * (boxSize + boxMargin);
+  }
+
+  static areCubeCoordsEqual(cube1, cube2) {
+    return cube1[0] === cube2[0] &&
+      cube1[1] === cube2[1] &&
+      cube1[2] === cube2[2];
   }
 }
--- Color/ColorCube3D/sketch.js
+++ Color/ColorCube3D/sketch.js
@@ -5,7 +5,7 @@
  * - [done] Draw axes
  * - [done] Draw axes ticks and tick labels
  * - [done] Allow cube selection (via keyboard)
- * - [] Show 2D slices (allow those to be interactive), which will change cursor in cube
+ * - [done] Show 2D slices (allow those to be interactive), which will change cursor in cube
  *      - Will need to run multiple p5 sketches though? With 2D slices rendered elsewhere? And immune to orbit control camera?
  * - [] When a cube is selected, highlight axis point as well (in white?) Maybe with tick or arrow?
  * - [] Show selected color in text (somewhere... maybe overlay as div)
